67c06fc23c882066f57de73ea61544fe24e2a761
Ryo Iseki <ryo.iseki345@gmail.com>
2018-04-26 13:35:15 +0900

    ランキング初期処理追加

--- TapGame/TableViewController.swift
+++ TapGame/TableViewController.swift
@@ -13,17 +13,26 @@ UITableViewDataSource, UITableViewDelegate{
     
     @IBOutlet weak var table: UITableView!
     
+    //let defaults = UserDefaults.standard
+    let label2ArrayForKey:String = "label2Key"
+    let label4ArrayForKey:String = "label4Key"
+    
     // section毎の画像配列
-    let label2Array: NSArray = ["02:00","04:00","04:30","05:00",
+    var label2Array:Array<Any> = []//Array(repeating: "?", count: 10)
+                                /*["02:00","04:00","04:30","05:00",
                                 "05:09","06:00","09:00","10:00",
-                                "10:00","10:00"]
-    let label4Array: NSArray = ["2018/04/20","2018/04/18","2018/04/20","2018/04/19",
+                                "10:00","10:00"]*/
+    var label4Array:Array<Any> = []//Array(repeating: "?", count: 10)
+                                /*["2018/04/20","2018/04/18","2018/04/20","2018/04/19",
                                 "2018/04/16","2018/04/13","2018/04/12","2018/04/10",
-                                "2018/04/10","2018/04/10"]
+                                "2018/04/10","2018/04/10"]*/
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        label2Array = readlabel2Array()
+        label4Array = readlabel4Array()
+        
         // Do any additional setup after loading the view.
     }
 
@@ -76,6 +85,46 @@ UITableViewDataSource, UITableViewDelegate{
         // Dispose of any resources that can be recreated.
     }
     
+    func endFunc() {
+        // アプリが終了する時
+       
+        //配列を保存する。
+        saveArray(result: label2Array)
+        saveArray(result: label4Array)
+    }
+
+    // 保存する
+    func saveArray(result: Array<Any>) {
+        let defaults = UserDefaults.standard
+        defaults.set(label2Array, forKey:label2ArrayForKey)
+        defaults.set(label4Array, forKey:label4ArrayForKey)
+        defaults.synchronize()
+    }
+    
+    // label2Array取得する
+    func readlabel2Array() -> Array<Any>  {
+        let defaults = UserDefaults.standard
+        if let aaa:Array = (defaults.object(forKey: label2ArrayForKey) as? Array<Any>)
+        {
+            return aaa
+        }
+        else
+        {
+            return Array(repeating: "?", count: 10)
+        }
+    }
+    // label4Array取得する
+    func readlabel4Array() -> Array<Any>  {
+        let defaults = UserDefaults.standard
+        if let aaa:Array = (defaults.object(forKey: label4ArrayForKey) as? Array<Any>)
+            {
+                return aaa
+        }
+        else
+        {
+            return Array(repeating: "?", count: 10)
+        }
+    }
 
     /*
     // MARK: - Navigation
